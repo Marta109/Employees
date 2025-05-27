6fa1f848004d5ff44b8ea9bfae182e4b3f3c53a7
Marta109 <marta.hayrapetyan.a@gmail.com>
2025-05-28 01:26:26 +0300

    init: start employees-app

---
+++ src/components/app-info/app-info.jsx
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employees App</h1>
+      <h2>Total number of employees:</h2>
+      <h2>Number of employees for promotion:</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
